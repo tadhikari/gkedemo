38dbfd5ae47ddd19e76388abd656a33f89aeb8d7
Other Guy <Other Guy@DESKTOP-TE88IGC.lan>
2025-03-10 16:01:52 -0500

    initial commit

---
+++ src/main/java/com/testing/demo/api/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.testing.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World";
+    }
+}
